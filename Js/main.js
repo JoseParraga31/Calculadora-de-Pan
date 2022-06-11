62b9fd9fd8ca025ff253074e5b91c371a08a7b2d
JoseParraga <joseparraga18@gmail.com>
2022-06-11 17:50:06 -0300

    agregamos objetos

--- Js/main.js
+++ Js/main.js
@@ -1,28 +1,37 @@
 //recetario interactivo
-
-let Harina = parseInt(prompt("introdusca el peso total del Harina en Gr"));
-let Agua = parseInt(prompt("introdusca el peso total del Agua en Gr"));
-let Levadura = parseInt(prompt("introdusca el peso total de Levadura en Gr"));
-let Sal = parseInt(prompt("introdusca el peso total de Sal en Gr"));
-let PesoTotal = 0;
-let HidratacionFinal = 0;
-
-function Hidratcion(n1,n2,n3){
-    HidratacionFinal = n1 * n2 / n3;
+function ingresedatos(mensaje){
+    return parseInt(prompt(mensaje))
 }
 function sumar(n1,n2,n3,n4){
-    PesoTotal = n1 + n2 + n3 + n4;
+    pesoTotal = n1 + n2 + n3 + n4;
 }
 function mostrar(mensaje){
     console.log(mensaje);
 }
-    console.log("tu lista de ingredientes es:");
-    console.log("harina"+"=" + Harina + "gr");
-    console.log("Agua"+"=" + Agua + "gr");
-    console.log("levadura"+"=" + Levadura + "gr");
-    console.log("sal"+"=" + Sal + "gr");
+function Hidratcion(n1,n2,n3){
+    hidratacionFinal = n1 * n2 / n3;
+}
+//funcion constructora para crear las recetas y capturarla en objetos
+class Receta {
+    constructor(harina, agua, levadura, sal ){
+        this.harina = harina;
+        this.agua = agua;
+        this.levadura = levadura;
+        this.sal = sal;
+    }
+    
+}
+let harina1 = ingresedatos("introdusca el peso total del Harina en Gr");
+let agua1 = ingresedatos("introdusca el peso total del Agua en Gr");
+let levadura1 = ingresedatos("introdusca el peso total de Levadura en Gr");
+let sal1 = ingresedatos("introdusca el peso total de Sal en Gr");
 
-sumar(Harina,Agua,Levadura,Sal);
-mostrar("El Peso total es de " + PesoTotal + "gr")
-Hidratcion(Agua,100,Harina)
-mostrar("La hidratacion total es de "+ HidratacionFinal+"%")
+const receta1 = new Receta(harina1,agua1,levadura1,sal1)
+mostrar(receta1)
+sumar(harina1,agua1,levadura1,sal1);
+mostrar("El Peso total es de " + pesoTotal + "gr")
+Hidratcion(agua1,100,harina1)
+mostrar("La hidratacion total es de "+ hidratacionFinal+"%")
+for(const propiedad in receta1){
+    console.log(propiedad + "="+ receta1[propiedad]+"Gr");
+} 
